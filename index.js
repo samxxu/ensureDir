985bcc1b389a2257c35a05dce7061080e71745f3
samxxu <samxxu@gmail.com>
2012-06-09 00:49:58 +0800

    reformat

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 /**
  * ensure a directory exists, create it recursively if not.
- * 
+ *
  * @param dir the directory you want to ensure it exists
  * @param mode refer to fs.mkdir()
  * @param callback
@@ -15,7 +15,9 @@ module.exports = function ensureDir(dir, mode, callback) {
   }
 
   mode = mode || 0777 & (~process.umask());
-  callback = callback || function (){};
+  ;
+  callback = callback || function () {
+  };
 
   _ensureDir(dir, mode, callback);
 }
@@ -23,16 +25,16 @@ module.exports = function ensureDir(dir, mode, callback) {
 function _ensureDir(dir, mode, callback) {
   var existsFunction = fs.exists || path.exists;
 
-  existsFunction(dir, function(exists) {
+  existsFunction(dir, function (exists) {
     if (exists) return callback(null);
 
     var current = path.resolve(dir);
     var parent = path.dirname(current);
 
-    _ensureDir(parent, mode, function(err) {
+    _ensureDir(parent, mode, function (err) {
       if (err)return callback(err);
 
-      fs.mkdir(current, mode, function(err) {
+      fs.mkdir(current, mode, function (err) {
         if (err)return callback(err);
         callback();
       });
